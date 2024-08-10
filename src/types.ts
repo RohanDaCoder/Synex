72a291ddb4df1d43d7f9b25efade049e072acc67
Rohan <102975537+RohanDaCoder@users.noreply.github.com>
2024-08-10 10:17:17 +0600

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,11 @@
+import { SlashCommandBuilder, CommandInteraction, Client } from "discord.js";
+
 export interface Config {
   devGuildIds: Array<string>;
   devUserIds: Array<string>;
 }
+
+export type Command = {
+  data: SlashCommandBuilder;
+  run: (args: { interaction: CommandInteraction; client: Client }) => Promise<void>;
+};
