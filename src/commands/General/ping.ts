afea0f378bc9577c0a44a4e5ecfcb876c2470205
Rohan <rohanbmgo2010@gmail.com>
2024-08-10 18:36:29 +0600

    Ping Command

--- src/commands/General/ping.ts
+++ src/commands/General/ping.ts
@@ -1,18 +1,20 @@
-import type {
-  CommandData,
-  SlashCommandProps,
-  CommandOptions,
-} from "commandkit";
+import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import type { Command } from "../../types";
+import { SlashCommandProps } from "commandkit";
 
-export const data: CommandData = {
-  name: "ping",
-  description: "Replies with Pong",
-};
+export default {
+  data: new SlashCommandBuilder().setName("ping").setDescription("Get My Ping"),
 
-export const run = ({ interaction }: SlashCommandProps) => {
-  interaction.reply("Pong!");
-};
-
-export const options: CommandOptions = {
-  // https://commandkit.js.org/typedef/CommandOptions
-};
+  run: async ({ interaction, client }: SlashCommandProps) => {
+    const embed = new EmbedBuilder()
+      .addFields({
+        name: "Client Ping",
+        value: `${client.ws.ping}`,
+      })
+      .setColor("Blurple")
+      .setTimestamp();
+    await interaction.reply({
+      embeds: [embed],
+    });
+  },
+} as Command;
