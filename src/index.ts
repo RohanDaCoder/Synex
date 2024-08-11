462c44c41960a54f7c138cc9083ce367f68edcc8
Rohan <rohanbmgo2010@gmail.com>
2024-08-11 13:50:31 +0600

    ah

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,60 @@
 import "dotenv/config";
-
-import { Client, IntentsBitField } from "discord.js";
-import { CommandKit } from "commandkit";
-
-import { join, dirname } from "node:path";
+import { Client, GatewayIntentBits, Events } from "discord.js";
 import { fileURLToPath } from "node:url";
+import path from "path";
+import fs from "fs";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const client = new Client({
   intents: [
-    IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.GuildMembers,
-    IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.MessageContent,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
   ],
 });
 
-new CommandKit({
-  client,
-  eventsPath: join(__dirname, "events"),
-  commandsPath: join(__dirname, "commands"),
-  bulkRegister: true,
-});
+async function loadEvents() {
+  try {
+    const eventNames = fs.readdirSync(path.join(__dirname, "events"));
+
+    for (const name of eventNames) {
+      const eventFiles = fs.readdirSync(path.join(__dirname, `events/${name}`));
+
+      for (const eventFile of eventFiles) {
+        const eventPath = path.join(__dirname, `events/${name}/${eventFile}`);
+
+        try {
+          // Dynamically import the event handler
+          const { default: runEvent } = await import(eventPath);
+
+          if (typeof runEvent === "function") {
+            // Type assertion to ensure correct event type
+            client.on(name as keyof typeof Events, (...props: any[]) =>
+              runEvent(client, ...props)
+            );
+          } else {
+            console.error(
+              `Event handler in file ${eventFile} is not a function.`
+            );
+          }
+        } catch (importError) {
+          console.error(
+            `Error importing event file ${eventFile}:`,
+            importError
+          );
+        }
+      }
+    }
+  } catch (error) {
+    console.error("Error loading events:", error);
+  }
+}
+
+// Call the loadEvents function to initialize event handlers
+loadEvents();
 
 client.login(process.env.TOKEN);
+
+export default client;
---
+++ src/test.js
@@ -0,0 +1,16 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+/**
+ *
+ * @param {import { Client } from "discord.js";} ahh
+ */
+function hi(ahh) {}
