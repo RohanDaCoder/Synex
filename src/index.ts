53d359258ff72705c3c13fccc89dd63a2be5341d
Rohan <rohanbmgo2010@gmail.com>
2024-08-13 22:14:18 +0600

    New Commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+console.clear()
 import "dotenv/config";
 import { Client, GatewayIntentBits, Events } from "discord.js";
 import { fileURLToPath } from "node:url";
 import path from "path";
 import fs from "fs";
+import eventHandler from "./handlers/eventHandler"
+import commandHandler from "./handlers/commandHandler";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -15,42 +18,8 @@ const client = new Client({
   ],
 });
 
-async function loadEvents() {
-  try {
-    const eventNames = fs.readdirSync(path.join(__dirname, "events"));
-
-    for (const name of eventNames) {
-      const eventFiles = fs.readdirSync(path.join(__dirname, `events/${name}`));
-
-      for (const eventFile of eventFiles) {
-        const eventPath = path.join(__dirname, `events/${name}/${eventFile}`);
-
-        try {
-          const { default: runEvent } = await import(eventPath);
-
-          if (typeof runEvent === "function") {
-            client.on(name as keyof typeof Events, (...props: any[]) =>
-              runEvent(client, ...props)
-            );
-          } else {
-            console.error(
-              `Event handler in file ${eventFile} is not a function.`
-            );
-          }
-        } catch (importError) {
-          console.error(
-            `Error importing event file ${eventFile}:`,
-            importError
-          );
-        }
-      }
-    }
-  } catch (error) {
-    console.error("Error loading events:", error);
-  }
-}
-
-loadEvents();
+eventHandler(client);
+commandHandler(client);
 
 client.login(process.env.TOKEN);
 
