ff85322dfe9407ae4d9ca6fc51ac3788b0251966
Rohan <rohanbmgo2010@gmail.com>
2024-09-16 20:59:06 +0600

    ey

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ const config = {
   devGuildIds: ["1271700025646387221"],
   devUserIds: ["922419431508938773"],
   token:
-    "MTI3MDMyMTcyMDI5MjU0MDQ0Ng.GpWDiz.KtUxbwGFhpyb7J0Deqfmy5FjiMADh-zCsLhCNQ",
+    "MTI3MDMyMTcyMDI5MjU0MDQ0Ng.GveAxu.oP0_KmC73BDMoDVCihb-BF-40MC1UaGj6aDTgI",
 } as Config;
 
 export default config;
