e3286c71b31686ebea999cfdd118bbd2d34a4a24
Rohan <rohanbmgo2010@gmail.com>
2024-09-16 20:56:21 +0600

    ahh

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ const config = {
   devGuildIds: ["1271700025646387221"],
   devUserIds: ["922419431508938773"],
   token:
-    "MTI3MDMyMTcyMDI5MjU0MDQ0Ng.G-yN2v.zQHLmVd4nvgsG0Olnnqj2vdhQZVIXrK9Pi-ebM",
+    "MTI3MDMyMTcyMDI5MjU0MDQ0Ng.GpWDiz.KtUxbwGFhpyb7J0Deqfmy5FjiMADh-zCsLhCNQ",
 } as Config;
 
 export default config;
