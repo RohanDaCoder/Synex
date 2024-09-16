98a28c992c98beefee98dce9cca5d82ef3cb5ba4
Rohan <rohanbmgo2010@gmail.com>
2024-09-16 21:02:52 +0600

    "Updated .gitignore, package.json, and src files"

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,7 @@ const config = {
   },
   devGuildIds: ["1271700025646387221"],
   devUserIds: ["922419431508938773"],
-  token:
-    "MTI3MDMyMTcyMDI5MjU0MDQ0Ng.GveAxu.oP0_KmC73BDMoDVCihb-BF-40MC1UaGj6aDTgI",
+  token: process.env.token,
 } as Config;
 
 export default config;
