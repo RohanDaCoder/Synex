cf851b057af9cb9fe187ef935377560918724c41
Rohan <rohanbmgo2010@gmail.com>
2024-11-15 10:49:35 +0600

    Add "platform: 'node'" to tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs"],
+  platform: "node",
   minify: true,
   splitting: false,
   sourcemap: false,
