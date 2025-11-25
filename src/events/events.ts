34e150f9003a70902bfe42dc9bee6762c637506b
Rohan <rohanbmgo2010@gmail.com>
2025-11-25 18:47:53 +0600

    Initial Commit of Rewrite (Empty)

--- src/events/events.ts
+++
@@ -1,9 +0,0 @@
-import { Event } from '../types';
-import runCommand from './interactionCreate/runCommand';
-import consoleLog from './ready/consoleLog';
-import joinEvent from './guildMemberJoinLeave/join';
-import leaveEvent from './guildMemberJoinLeave/leave';
-
-const events: Event[] = [runCommand, consoleLog, joinEvent, leaveEvent];
-
-export default events;
